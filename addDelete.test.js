88086ddc0f3000766ed892c628ab82794d92f977
Mohamed Jadib <jadibdev@gmail.com>
2022-06-08 21:36:19 +0100

    Add unit tests for edit & update functionality

--- addDelete.test.js
+++ addDelete.test.js
@@ -688,3 +688,208 @@ describe('Adds and removes only 1 li element', () => {
     expect(tasksUl.innerHTML).toBe('');
   });
 });
+
+describe('Testing content editing and updating', () => {
+  test('edit functionality works!', () => {
+    /*
+      ARRANGE
+      --------
+    */
+    const handleUserInput = () => {
+      const inputEl = document.getElementById('task');
+      inputEl.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+          const taskObject = {
+            description: event.target.value,
+            completed: false,
+            index: tasks.length,
+          };
+          // localStorage.setItem('tasks', JSON.stringify(tasks));
+          tasks.push(taskObject);
+          localStorage.setItem('tasks', JSON.stringify(tasks));
+
+          const li = document.createElement('li');
+          li.id = `li${taskObject.index}`;
+
+          const div = document.createElement('div');
+          div.classList.add('liDiv');
+
+          const input = document.createElement('input');
+          input.type = 'checkbox';
+
+          const p = document.createElement('p');
+          p.textContent = taskObject.description;
+
+          const ellipsis = document.createElement('i');
+          ellipsis.id = `ellipsis${taskObject.index}`;
+          const ellipsisClasses = ['fa-solid', 'fa-ellipsis-vertical'];
+          ellipsis.classList.add(...ellipsisClasses);
+
+          const trash = document.createElement('i');
+          trash.id = `trash${taskObject.index}`;
+          const trashClasses = ['fa-solid', 'fa-trash-can', 'disappear'];
+          trash.classList.add(...trashClasses);
+
+          div.append(input);
+          div.append(p);
+          li.append(div);
+
+          li.append(ellipsis);
+          li.append(trash);
+
+          document.querySelector('ul').append(li);
+          event.target.value = '';
+
+          const edit = () => {
+            li.contentEditable = true;
+            li.style.backgroundColor = 'yellow';
+            ellipsis.classList.add('disappear');
+            trash.classList.remove('disappear');
+          };
+
+          ellipsis.addEventListener('click', edit);
+
+          const update = (event) => {
+            if (event.key === 'Enter') {
+              li.contentEditable = false;
+              li.style.backgroundColor = 'white';
+              trash.classList.add('disappear');
+              ellipsis.classList.remove('disappear');
+            }
+          };
+
+          li.addEventListener('keypress', update(event));
+
+          trash.addEventListener('click', () => {
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            li.remove();
+            tasks.splice(taskObject.index, 1);
+            for (let i = 0; i < tasks.length; i += 1) {
+              tasks[i].index = i;
+            }
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+          });
+
+          input.addEventListener('click', () => {
+            p.classList.toggle('lineThrough');
+            li.classList.toggle('completed');
+            if (tasks[taskObject.index].completed === false) {
+              tasks[taskObject.index].completed = true;
+            } else {
+              tasks[taskObject.index].completed = false;
+            }
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+          });
+
+          event.preventDefault();
+
+          // ACT
+          edit();
+
+          // ASSERT
+          expect(li.contentEditable).toBe(true);
+        }
+      });
+    };
+  });
+  test('update functionality works!', () => {
+    /*
+      ARRANGE
+      --------
+    */
+    const handleUserInput = () => {
+      const inputEl = document.getElementById('task');
+      inputEl.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+          const taskObject = {
+            description: event.target.value,
+            completed: false,
+            index: tasks.length,
+          };
+          // localStorage.setItem('tasks', JSON.stringify(tasks));
+          tasks.push(taskObject);
+          localStorage.setItem('tasks', JSON.stringify(tasks));
+
+          const li = document.createElement('li');
+          li.id = `li${taskObject.index}`;
+
+          const div = document.createElement('div');
+          div.classList.add('liDiv');
+
+          const input = document.createElement('input');
+          input.type = 'checkbox';
+
+          const p = document.createElement('p');
+          p.textContent = taskObject.description;
+
+          const ellipsis = document.createElement('i');
+          ellipsis.id = `ellipsis${taskObject.index}`;
+          const ellipsisClasses = ['fa-solid', 'fa-ellipsis-vertical'];
+          ellipsis.classList.add(...ellipsisClasses);
+
+          const trash = document.createElement('i');
+          trash.id = `trash${taskObject.index}`;
+          const trashClasses = ['fa-solid', 'fa-trash-can', 'disappear'];
+          trash.classList.add(...trashClasses);
+
+          div.append(input);
+          div.append(p);
+          li.append(div);
+
+          li.append(ellipsis);
+          li.append(trash);
+
+          document.querySelector('ul').append(li);
+          event.target.value = '';
+
+          ellipsis.addEventListener('click', () => {
+            li.contentEditable = true;
+            li.style.backgroundColor = 'yellow';
+            ellipsis.classList.add('disappear');
+            trash.classList.remove('disappear');
+          });
+
+          const update = (event) => {
+            if (event.key === 'Enter') {
+              li.contentEditable = false;
+              li.style.backgroundColor = 'white';
+              trash.classList.add('disappear');
+              ellipsis.classList.remove('disappear');
+            }
+          };
+
+          li.addEventListener('keypress', update(event));
+
+          trash.addEventListener('click', () => {
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            li.remove();
+            tasks.splice(taskObject.index, 1);
+            for (let i = 0; i < tasks.length; i += 1) {
+              tasks[i].index = i;
+            }
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+          });
+
+          input.addEventListener('click', () => {
+            p.classList.toggle('lineThrough');
+            li.classList.toggle('completed');
+            if (tasks[taskObject.index].completed === false) {
+              tasks[taskObject.index].completed = true;
+            } else {
+              tasks[taskObject.index].completed = false;
+            }
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+          });
+
+          event.preventDefault();
+
+          // ACT
+          update(event);
+
+          // ASSERT
+          expect(li.contentEditable).toBe(false);
+        }
+      });
+    };
+  });
+});
